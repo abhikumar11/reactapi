969d372c8ce3a74aca24fa7b3c2d8f5ed0aea545
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-31 13:25:52 +0530

    initial commit

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,16 +1,34 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 const Api = () => {
-        useEffect(() => {
-          axios.get("https://jsonplaceholder.typicode.com/photos")
-          .then((res)=>console.log(res.data))
-        }, [])
-        
-  return (
-    <div>
-        <h1>API</h1>
-    </div>
-  )
+    const [apidata,setApiData]=useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then((res)=>setApiData(res.data));
+    }, [])
+
+    return (
+        <div>
+            <h1>API</h1>
+            <table border="2px">
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Url</th>
+                </tr>
+                {
+                    apidata.map((item)=>(
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.url}</td>
+                        </tr>
+                    ))
+                }
+            </table>
+        </div>
+    )
 }
 
 export default Api
