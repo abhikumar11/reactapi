b08cf099b5b015789951ff0ce70790f5fbf012c3
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-31 22:56:58 +0530

    initial commit

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,34 +1,34 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect,useState } from "react";
+
+
 const Api = () => {
-    const [apidata,setApiData]=useState([]);
+     const [apidata, setApiData] = useState([]);
 
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/photos')
-        .then((res)=>setApiData(res.data));
-    }, [])
+     useEffect(() => {
+          axios.get("https://jsonplaceholder.typicode.com/photos")
+          .then((res) =>setApiData(res.data));
+     }, []);
 
-    return (
-        <div>
-            <h1>API</h1>
-            <table border="2px">
-                <tr>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Url</th>
-                </tr>
-                {
-                    apidata.map((item)=>(
-                        <tr>
-                            <td>{item.id}</td>
-                            <td>{item.title}</td>
-                            <td>{item.url}</td>
-                        </tr>
-                    ))
-                }
-            </table>
-        </div>
-    )
-}
+     return (
+          <div>
+               <h1>API</h1>
+               <table border="2px">
+                    <tr>
+                         <th>Id</th>
+                         <th>Title</th>
+                         <th>Url</th>
+                    </tr>
+                    {apidata.map((item) => (
+                         <tr>
+                              <td>{item.id}</td>
+                              <td>{item.title}</td>
+                              <td>{item.url}</td>
+                         </tr>
+                    ))}
+               </table>
+          </div>
+     );
+};
 
-export default Api
+export default Api;
