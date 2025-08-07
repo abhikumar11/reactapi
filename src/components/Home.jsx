d15fab5996cdf59cb2296f0900d090a2f61df8a9
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-08-07 12:37:32 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,19 @@ import React, { useRef } from 'react'
 import { toast} from 'react-toastify';
 const Home = () => {
     const myref=useRef();
+
     const handleClick=()=>{
         myref.current.value="";
         myref.current.focus();
     }
+    const handleHeading=()=>{
+        myref.current.style.color="skyblue"
+    }
   return (
     <div>
-        <h1>This is home page</h1>
+        <h1 ref={myref}>This is home page</h1>
+        <button onClick={()=>handleHeading}>Change</button>
+        <br />
         <button onClick={()=>toast.success("")}>Click Me</button>
         <br /><br />
         <input type="text" ref={myref}/>
